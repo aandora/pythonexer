7458ade85028ab9a8eb2de1e702d0e9281f6b17b
andrew andora <andrew.andora@seer-technologies.com>
2015-10-13 12:50:49 +0800

    removing import sys

--- aandora.py
+++ aandora.py
@@ -1,46 +1,54 @@
-import sys
-
 def half_triangle(number):
+	triangle = ""
 	for i in xrange(1, number+1):
 		for j in xrange(0, i):
-			sys.stdout.write("1")
-		print ""
+			triangle += "1"
+		triangle += "\n"
+
+	print triangle
 
 print "\nHALF TRIANGLE\n"
 half_triangle(5)
 
 def pyramid_a(number):
+	triangle = ""
 	width = number*4+1;
 	median = number*2;
 	for i in xrange(0, number):
 		for j in xrange(0, width):
 			diff = abs(j-median)
 			if(diff>2*i):
-				sys.stdout.write(" ")
+				triangle += " "
 			else:
 				if(diff%2==0):
-					sys.stdout.write("1")
+					triangle += "1"
 				else:
-					sys.stdout.write(" ")
-		print ""
+					triangle += " "
+		triangle += "\n"
+
+	print triangle
 
 print "\nPYRAMID A\n"
 pyramid_a(5)
 
 def pyramid_b(number):
+	triangle = ""
 	width = number*2-1;
 	median = number - 1;
 	for i in xrange(0, number):
 		for j in xrange(0, width):
 			diff = abs(j-median)
 			if(diff>i):
-				sys.stdout.write(" ")
+				triangle += " "
 			else:
 				if(diff%2 == i%2):
-					sys.stdout.write("1")
+					triangle += "1"
 				else:
-					sys.stdout.write(" ")
-		print ""
+					triangle += " "
+
+		triangle += "\n"
+
+	print triangle
 
 print "\nPYRAMID B\n"
 pyramid_b(5)
