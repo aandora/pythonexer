ea34ae5b21685861217cd0834574f7fe5af84de4
andrew andora <andrew.andora@seer-technologies.com>
2015-10-14 10:15:05 +0800

    adding generator.py exercise

---
+++ generator.py
@@ -0,0 +1,18 @@
+set = xrange(0, 5000)
+
+def fibonacci(n):
+	prev1 = 1
+	prev2 = 0
+	for i in n:
+		if(i == 0):
+			yield prev2
+		elif(i==1):
+			yield prev1
+		else:
+			sum = prev1 + prev2
+			prev2 = prev1
+			prev1 = sum
+			yield sum
+
+for x in fibonacci(set):
+	print x
